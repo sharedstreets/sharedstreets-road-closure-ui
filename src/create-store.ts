5a277adbb8278d76d17e54f3333f529b2720dfdf
indraneel <i.m.indraneel@gmail.com>
2019-02-04 15:50:54 -0500

    fix #15 allowing app to load without redux devtool installed

--- src/create-store.ts
+++ src/create-store.ts
@@ -11,9 +11,9 @@ export default () => {
         rootReducer,
         compose (
             applyMiddleware(thunk),
-            (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__({
+            (window as any).__REDUX_DEVTOOLS_EXTENSION__ ? (window as any).__REDUX_DEVTOOLS_EXTENSION__({
                 trace: true
-            })
+            }) : (a: any) => a
         )
     );
 
