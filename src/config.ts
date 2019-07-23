3f3b467350e92323b20f6ea20922a7dbab22dc8b
indraneel <i.m.indraneel@gmail.com>
2019-07-23 11:29:43 -0400

    add app config

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,13 @@
 // tslint:disable-next-line
-const config = require('./config.json');
+const config = require('./app.config.json');
 
 export const isAppRunningLocally = () => {
     // TODO - check NODE_ENV
-    return config && config.directory;
+    return process.env.REACT_APP_LOCAL_SERVER;
+    // return config && config.local_server;
 }
 
 export const AppStorageDirectory = config.directory;
 export const AppExtent = config.extent;
-export const AppPort = config.server_port ? config.server_port : 3001;
+export const AppPort = config.server_port;
 export const AppBaseServerURL = config.base_server_url ? config.base_server_url : 'http://localhost';
