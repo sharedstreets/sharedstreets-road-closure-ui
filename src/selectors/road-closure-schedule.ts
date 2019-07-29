c7d95e7f4e4508bba3ff8bf0b33b945b80b85086
indraneel <i.m.indraneel@gmail.com>
2019-07-29 14:48:35 -0400

    short circuit schedule-to-calendar selector if no schedule exists

--- src/selectors/road-closure-schedule.ts
+++ src/selectors/road-closure-schedule.ts
@@ -7,25 +7,27 @@ import { IRoadClosureState } from 'src/store/road-closure';
 
 export const calendarIntervalSelector = (state: IRoadClosureState) => {
     const output: any[] = [];
-    Object.keys(state.currentItem.properties.schedule).forEach((week) => {
-        Object.keys(state.currentItem.properties.schedule[week]).forEach((day) => {
-            state.currentItem.properties.schedule[week][day].forEach((scheduleBlock: IRoadClosureScheduleBlock) => {
-                const startInterval = moment()
-                        .day(day)
-                        .week(parseInt(week, 10))
-                        .hour(parseInt(scheduleBlock.startTime.split(":")[0], 10))
-                        .minutes(parseInt(scheduleBlock.startTime.split(":")[1], 10));
-                const endInterval = moment()
-                        .day(day)
-                        .week(parseInt(week, 10))
-                        .hour(parseInt(scheduleBlock.endTime.split(":")[0], 10))
-                        .minutes(parseInt(scheduleBlock.endTime.split(":")[1], 10));
-                output.push({
-                    end: endInterval,
-                    start: startInterval,
-                });
+    if (state.currentItem.properties.schedule) {
+        Object.keys(state.currentItem.properties.schedule).forEach((week) => {
+            Object.keys(state.currentItem.properties.schedule[week]).forEach((day) => {
+                state.currentItem.properties.schedule[week][day].forEach((scheduleBlock: IRoadClosureScheduleBlock) => {
+                    const startInterval = moment()
+                            .day(day)
+                            .week(parseInt(week, 10))
+                            .hour(parseInt(scheduleBlock.startTime.split(":")[0], 10))
+                            .minutes(parseInt(scheduleBlock.startTime.split(":")[1], 10));
+                    const endInterval = moment()
+                            .day(day)
+                            .week(parseInt(week, 10))
+                            .hour(parseInt(scheduleBlock.endTime.split(":")[0], 10))
+                            .minutes(parseInt(scheduleBlock.endTime.split(":")[1], 10));
+                    output.push({
+                        end: endInterval,
+                        start: startInterval,
+                    });
+                })
             })
-        })
-    });
+        });
+    }
     return output;
 }
