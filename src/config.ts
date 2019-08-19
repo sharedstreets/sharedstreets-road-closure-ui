b72cdd9b356c60b82548e5e9404bb71cfa7f34bb
indraneel <i.m.indraneel@gmail.com>
2019-08-19 15:26:52 -0400

    Update app.config.template.json
    add extent + org name to app config

--- src/config.ts
+++ src/config.ts
@@ -9,3 +9,4 @@ export const AppStorageDirectory = config.directory;
 export const AppExtent = config.extent;
 export const AppPort = config.server_port;
 export const AppBaseServerURL = config.base_server_url ? config.base_server_url : 'http://localhost';
+export const AppOrgName = config.org_name ? config.org_name : null;
