b39b92ec6d44634b729bab88d2866eca4961199b
indraneel <i.m.indraneel@gmail.com>
2019-09-24 15:55:07 -0400

    update baseurl and url paths

--- cypress/integration/load_closure.js
+++ cypress/integration/load_closure.js
@@ -1,11 +1,11 @@
 describe('My First Test', function() {
     it('Visit explore', function() {
-        cy.visit('/test/explore');
+        cy.visit('/Test/explore');
     });
     it('Visit explore, clicks on link, goes to editor', function() {
-        cy.visit('/test/explore');
+        cy.visit('/Test/explore');
         cy.contains("Open...").click();
         cy.contains("in map editor").click();
-        cy.url().should('include', '/test/edit');
+        cy.url().should('include', '/Test/edit');
     });
 });
